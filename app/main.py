8d6fde537558796b0fd24b46be8938c56ce0d818
Naman Chauhan <chauhannaman98@gmail.com>
2020-10-31 22:29:27 +0530

    update home API

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from datetime import date
 from flask_restful import Resource, Api
 from app.api.tv_shows import Top250
 
@@ -6,20 +7,22 @@ app = Flask(__name__)
 api = Api(app)
 
 
-# A simple test api
-class HelloWorld(Resource):
+class Home(Resource):
     def __init__(self):
         pass
 
     def get(self):
         return {
             'status': True,
-            'response': 'Hello World!',
-            'source': 'https://github.com/chauhannaman98'
+            'date': str(date.today().strftime("%b-%d-%Y")),
+            'source': 'https://github.com/chauhannaman98/IMDb-API',
+            'api-services-available': {
+                'Top25 TV Shows': request.base_url+'tv-shows/top250',
+            }
         }
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Home, '/')
 api.add_resource(Top250, '/tv-shows/top250')
 
 if __name__ == '__main__':
