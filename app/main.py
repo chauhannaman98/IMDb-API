1270c0b763bcb63b77abf29e4d4530a6fd562bc1
Naman Chauhan <chauhannaman98@gmail.com>
2020-11-01 20:44:25 +0530

    added SearchbyTitle route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from datetime import date
 from flask_restful import Resource, Api
+from app.api.search import SearchByTitle
 from app.api.tv_shows import Top250
 
 app = Flask(__name__)
@@ -17,13 +18,15 @@ class Home(Resource):
             'date': str(date.today().strftime("%b-%d-%Y")),
             'source': 'https://github.com/chauhannaman98/IMDb-API',
             'api-services-available': {
+                'Search by title': request.base_url+'search?q=TitleHere',
                 'Top25 TV Shows': request.base_url+'tv-shows/top250',
             }
         })
 
 
 api.add_resource(Home, '/')
-api.add_resource(Top250, '/tv-shows/top250')
+api.add_resource(SearchByTitle, '/search', endpoint='search')
+api.add_resource(Top250, '/tv-shows/top250', endpoint='top250')
 
 if __name__ == '__main__':
     app.run(debug=True)
