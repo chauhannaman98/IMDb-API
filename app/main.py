d7d308d60559492f18f0411495cf037307840b64
Naman Chauhan <chauhannaman98@gmail.com>
2020-11-12 15:59:14 +0530

    integrated sentry

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
+import sentry_sdk
 from flask import Flask, jsonify, request
 from datetime import date
 from flask_restful import Resource, Api
 from app.api.search import Search
 from app.api.tv_shows import Top250
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init(
+    dsn="https://95ce977722294eae9055d6af5a69b111@o475339.ingest.sentry.io/5513251",
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,6 +31,11 @@ class Home(Resource):
                 'Top25 TV Shows': request.base_url+'tv-shows/top250',
             }
         })
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
 
 
 api.add_resource(Home, '/')
