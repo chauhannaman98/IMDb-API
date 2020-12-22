4c640a45c4995f7de25dc1546ecbd864950a2cd0
Naman Chauhan <chauhannaman98@gmail.com>
2020-12-22 21:52:41 +0530

    Added name type query for getById

--- app/api/get.py
+++ app/api/get.py
@@ -7,13 +7,26 @@ try:
 except Exception as e:
     print('Caught exception while importing: {}'.format(e))
 
+
 class Get(Resource):
     def get(self):
-        response_obj = getById.GetById()
-        response = response_obj.getById()
-        print(response)
-        print(type(response))
+
+        args = request.args
+        q_type = args['type']
+
+        if(q_type == 'title'):
+            response_obj = getById.GetTitleById()
+            response = response_obj.getById()
+            success = True
+        elif(q_type == 'name'):
+            response_obj = getById.GetNameById()
+            response = response_obj.getById()
+            success = True
+        else:
+            response = 'query type not supported'
+            success = False
+
         return jsonify({
-            'success': True,
+            'success': success,
             'response': response
         })
--- app/scrappers/get/getById.py
+++ app/scrappers/get/getById.py
@@ -5,7 +5,31 @@ try:
 except Exception as e:
     print('Caught exception while importing: {}'.format(e))
 
-class GetById:
+BASE_URL = "https://www.imdb.com/"
+
+
+class GetTitleById:
     def getById(self):
-        response = {}
+        response = {'title': 'title'}
         return response
+
+
+class GetNameById:
+    def getById(self):
+        response = {'name': 'name'}
+        return response
+
+
+def main():
+    title_id = 'intersteller'
+    get_title = GetTitleById()
+    results = get_title.getById()
+    print(results)
+    name_id = 'intersteller'
+    get_name = GetNameById()
+    results = get_name.getById()
+    print(results)
+
+
+if __name__ == '__main__':
+    main()
